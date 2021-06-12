20b5cd485f21e1f020f785a19e68f3f92fc1c54c
Ryan Pitts <ryanpitts1@gmail.com>
2021-06-12 17:13:52 -0500

    Finished new site

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,47 +1,49 @@
 $(function() {
-    
+
     "use strict";
-    
+
     //===== Prealoder
-    
+
     $(window).on('load', function(event) {
         $('#preloader').delay(500).fadeOut(500);
     });
-    
-    
-    //===== Mobile Menu 
-    
+
+
+    //===== Mobile Menu
+
     $(".navbar-toggler").on('click', function() {
         $(this).toggleClass('active');
         $(".navbar-collapse").toggleClass("show");
     });
-    
+
     $(".navbar-nav a").on('click', function() {
         $(".navbar-toggler").removeClass('active');
     });
-    
-    
+
+
     //===== close navbar-collapse when a  clicked
-    
+
     $(".navbar-nav a").on('click', function () {
         $(".navbar-collapse").removeClass("show");
     });
-    
-    
+
+
     //===== Sticky
-    
-    $(window).on('scroll',function(event) {    
+
+    $(window).on('scroll',function(event) {
         var scroll = $(window).scrollTop();
         if (scroll < 10) {
             $(".navigation").removeClass("sticky");
+            $(".navigation a").removeClass('force-text-secondary');
         }else{
             $(".navigation").addClass("sticky");
+            $(".navigation a").addClass('force-text-secondary');
         }
     });
-    
-    
+
+
     //===== Section Menu Active
-    
+
     var scrollLink = $('.page-scroll');
         // Active link switching
         $(window).scroll(function() {
@@ -57,16 +59,16 @@ $(function() {
           }
         });
     });
-    
-    
+
+
     //====== Magnific Popup
-    
+
     $('.video-popup').magnificPopup({
         type: 'iframe'
         // other options
     });
-    
-    
+
+
     //===== Slick
 
     $('.testimonial-active').slick({
@@ -100,10 +102,10 @@ $(function() {
             }
         ]
     });
-    
-    
+
+
     //===== Back to top
-    
+
     // Show or hide the sticky footer button
     $(window).on('scroll', function(event) {
         if($(this).scrollTop() > 600){
@@ -112,33 +114,33 @@ $(function() {
             $('.back-to-top').fadeOut(200)
         }
     });
-    
-    
+
+
     //Animate the scroll to yop
     $('.back-to-top').on('click', function(event) {
         event.preventDefault();
-        
+
         $('html, body').animate({
             scrollTop: 0,
         }, 1500);
     });
-    
-    
-    //===== 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+
+    //=====
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
